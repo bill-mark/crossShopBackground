5e637d447adbaaff021e2e6ae14b33ac12b7449f
benyl-nie <18355263859@163.com>
2022-01-06 21:27:45 +0800

    设备标签

--- src/api/tag.js
+++ src/api/tag.js
@@ -1,9 +1,33 @@
 import request from "@/utils/request";
 
 export const fetchTagList = (data) => {
-    return request({
-        url: "/client_v1/device/device-tag-list",
-        method: "GET",
-        params: {...data}
-    })
+  return request({
+    url: "/client_v1/device/device-tag-list",
+    method: "GET",
+    params: {...data}
+  })
+}
+
+export const addTag = (data) => {
+  return request({
+    url: "/client_v1/device/create-device-tag",
+    method: "POST",
+    data: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json; charset=utf-8',
+      'Accept': 'application/json'
+    }
+  })
+}
+
+export const updateTag = (data) => {
+  return request({
+    url: "/client_v1/device/update-device-tag",
+    method: "POST",
+    data: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json; charset=utf-8',
+      'Accept': 'application/json'
+    }
+  })
 }
--- src/views/equipment/tagList.vue
+++ src/views/equipment/tagList.vue
@@ -7,6 +7,7 @@
         </a-form-model-item>
       </a-form-model>
       <a-button type="primary" @click="fetch" class="search_btn">查询</a-button>
+      <a-button type="primary" @click="add" class="search_btn">添加设备标签</a-button>
     </div>
     <div class="tag_panel">
       <a-table
@@ -17,12 +18,27 @@
         :loading="loading"
         @change="handleTableChange"
       >
+        <template slot="operation" slot-scope="text, record">
+          <div class="editable-row-operations">
+            <span><a @click="() => edit(record)">编辑</a></span>
+          </div>
+        </template>
       </a-table>
     </div>
+    <a-modal v-model="showDialog" title="添加/新增设备标签" @ok="handleOk">
+      <a-form-model :model="addForm" :label-col="labelCol" :wrapper-col="wrapperCol">
+        <a-form-model-item label="标签名称">
+          <a-input v-model="addForm.tag" />
+        </a-form-model-item>
+        <a-form-model-item label="排序" v-if="!isEdit">
+          <a-input v-model="addForm.sort" />
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
   </div>
 </template>
 <script>
-import {fetchTagList} from '@/api/tag'
+import {addTag, updateTag, fetchTagList} from '@/api/tag'
 
 const columns = [{
   title: '标签ID',
@@ -39,6 +55,10 @@ const columns = [{
 },{
   title: '标签创建时间',
   dataIndex: 'created_at'
+}, {
+  title: '操作',
+  dataIndex: 'operation',
+  scopedSlots: { customRender: 'operation' },
 }];
 export default {
   name: 'tagList',
@@ -54,7 +74,18 @@ export default {
       columns: columns,
       list: [],
       pagination: {},
-      loading: false
+      loading: false,
+      showDialog: false,
+      addForm: {
+        tag: null,
+        sort: 0
+      },
+      common: {
+        version: '1.0.0',
+        mask: 'dev',
+        platform: 1
+      },
+      isEdit: false,
     }
   },
   mounted(){
@@ -62,6 +93,8 @@ export default {
   },
   methods: {
     async fetch() {
+      // this.list = [{id: '231', tag: 'tag', status: '1', sort: '1', created_at: '1231231'}]; //mock
+
       this.loading = true;
       const {data: list, total} = await fetchTagList({
         ...this.query,
@@ -71,22 +104,11 @@ export default {
       });
       // console.log('result', result);
       const pagination = { ...this.pagination };
-      // Read total count from server
-      // pagination.total = data.totalCount;
       pagination.total = total;
       this.loading = false;
       this.list = list;
       this.pagination = pagination;
 
-      // .then(({ data }) => {
-      //   const pagination = { ...this.pagination };
-      //   // Read total count from server
-      //   // pagination.total = data.totalCount;
-      //   pagination.total = 200;
-      //   this.loading = false;
-      //   this.data = data.list;
-      //   this.pagination = pagination;
-      // });
     },
     handleTableChange(pagination) {
       console.log(pagination);
@@ -96,6 +118,29 @@ export default {
       this.pagination = pager;
       // const {tag, pagesize} = this.query;
       this.fetch();
+    },
+    add: function() {
+      this.showDialog = true;
+      this.isEdit = false;
+    },
+    async handleOk() {
+      let data;
+      if (this.isEdit) {
+        data = await updateTag({...this.addForm, ...this.common});
+      } else {
+        data = await addTag({...this.addForm, ...this.common});
+      }
+      
+      if (data && data.data) {
+        this.$messgae.success('添加/编辑成功');
+      }
+      this.showDialog = false;
+      this.isEdit = false;
+    },
+    edit: function({id, tag}) {
+      this.isEdit = true;
+      this.showDialog = true;
+      this.addForm = {id, tag};
     }
   }
 }
