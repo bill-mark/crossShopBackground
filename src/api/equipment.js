37f99c7841bceacbfe913621e147a2ea582c98e7
benyl-nie <18355263859@163.com>
2022-01-10 16:45:52 +0800

    eq

--- src/api/equipment.js
+++ src/api/equipment.js
@@ -4,10 +4,6 @@ export const getList = (data) => {
   return request({
     url: "/client_v1/device",
     method: "POST",
-    data: JSON.stringify(data),
-    headers: {
-      'Content-type': 'application/json; charset=utf-8',
-      'Accept': 'application/json'
-    }
+    data
   })
 }
